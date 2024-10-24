f9111a7be98e61025cc2033b39b334a143fb8825
aalimsahin <alimsahin0007@gmail.com>
2024-10-24 18:20:37 +0300

    feat: added stringUtils tests

--- deploy/deploy-zk-email.ts
+++ deploy/deploy-zk-email.ts
@@ -5,6 +5,9 @@ const factoryAddress = "0xeAf2aCE9a926Ab3a155dCbeDe74A1541d471E286";
 const verifier = "0xfCb6e1BAf589dc25f1224998eA0a256e8A542A29";
 const dkimRegistry = "0x140ba2dDf1D49B4BC4Bce0D5816b424584703B53";
 const emailAuthImpl = "0xe4E21f8dA20c66FB05feef1668e6a8529A9Ac3C9";
+const bytecodeHash =
+  "0x01000081e1a320a6d3dc2345f32b97538854bcf0c95ef3ad8e2a17227f9104f6";
+const minimumDelay = 0;
 
 export default async function (): Promise<void> {
   const wallet = getWallet(hre);
@@ -29,10 +32,6 @@ export default async function (): Promise<void> {
   await deployContract(hre, proxyArtifactName, [emailAuthImpl, "0x"], {
     silent: true,
   });
-
-  const minimumDelay = 0;
-  const bytecodeHash =
-    "0x01000081e1a320a6d3dc2345f32b97538854bcf0c95ef3ad8e2a17227f9104f6";
 
   const EmailRecoveryModuleArtifactName = "EmailRecoveryModule";
   const emailRecoveryModule = await deployContract(
